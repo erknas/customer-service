8da20e5c0bc73e114e891ddd84f2d08180e31cef
Aleksandr Vinogradov <vin89441@gmail.com>
2025-10-30 17:35:41 +0300

    domain models

--- internal/models/customer.go
+++ internal/models/customer.go
@@ -1,14 +1,35 @@
 package models
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+const layout = "2006-01-02"
+
 type Customer struct {
 	ID        uuid.UUID
 	Username  string
 	Fullname  string
+	City      string
+	BirthDate time.Time
+	IsActive  bool
 	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewCustomer(userName, fullName, city, birthDateStr string) (Customer, error) {
+	birthDate, err := time.Parse(layout, birthDateStr)
+	if err != nil {
+		return Customer{}, fmt.Errorf("invalide birth date format")
+	}
+
+	return Customer{
+		Username:  userName,
+		Fullname:  fullName,
+		City:      city,
+		BirthDate: birthDate,
+	}, nil
 }
