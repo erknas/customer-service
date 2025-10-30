1f10af5f97b13118f98b1e0a87194df627ae905f
Aleksandr Vinogradov <vin89441@gmail.com>
2025-10-30 17:36:26 +0300

    usecase

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -4,18 +4,36 @@ import (
 	"context"
 
 	"github.com/erknas/customer-service/internal/models"
+	"go.uber.org/zap"
 )
 
-type UserRepository interface {
-	InsertCustomer(ctx context.Context, customer *models.Customer) (*models.Customer, error)
+type customerRepository interface {
+	InsertCustomer(ctx context.Context, customer models.Customer) (models.Customer, error)
 }
 
 type customerUseCase struct {
-	repo UserRepository
+	repo   customerRepository
+	logger *zap.Logger
 }
 
-func New(repo UserRepository) *customerUseCase {
+func New(repo customerRepository, logger *zap.Logger) *customerUseCase {
 	return &customerUseCase{
-		repo: repo,
+		repo:   repo,
+		logger: logger,
 	}
+}
+
+func (c *customerUseCase) RegisterCustomer(ctx context.Context, userName, fullName, city, birthDateStr string) (models.Customer, error) {
+	customer, err := models.NewCustomer(userName, fullName, city, birthDateStr)
+	if err != nil {
+		return models.Customer{}, err
+	}
+
+	result, err := c.repo.InsertCustomer(ctx, customer)
+	if err != nil {
+		c.logger.Error("failed to insert customer", zap.Error(err))
+		return models.Customer{}, err
+	}
+
+	return result, nil
 }
